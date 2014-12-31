8cce464257549902da8a44d3f102effa397e9311
Rajesh Pillai <pillai.rajesh@gmail.com>
2014-12-31 12:48:02 +0530

    01 A Good First Program

---
+++ ex1.rb
@@ -0,0 +1,12 @@
+=begin
+xercise 1: A Good First Program
+You should have spent a good amount of time in Exercise 0 learning how to install a text editor, run the text editor, run the Terminal, and work with both of them. If you haven't done that, then do not go on. You will not have a good time. This is the only time I'll start an exercise with a warning that you should not skip or get ahead of yourself.
+=end
+
+puts "Hello World!"
+puts "Hello Again!"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
